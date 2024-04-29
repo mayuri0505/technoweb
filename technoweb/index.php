a8e4610898d11cde4ee4107782c2be8034b2c2af
mayuri0505 <127719650+mayuri0505@users.noreply.github.com>
2024-04-29 18:47:47 +0530

    technoweb

---
+++ technoweb/index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!---------css link---------->
+    <link rel="stylesheet" href="style.css">
+    <!---------Animation link---------->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <!---------Bootstrap cdn link---------->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!---------Font Awsome link---------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Techno web</title>
+</head>
+<body>
+
+<!----------------header-------------------->
+
+<header id="home">
+<nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand fs-1 px-4" href="#">Techno-<span class="text-primary">web</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto fs-5 text-center">
+        <li class="nav-item">
+          <a class="nav-link active text-primary" aria-current="page" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Service
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+            <li><a class="dropdown-item" href="#service">Performance Marketing</a></li>
+            <li><a class="dropdown-item" href="#service">Prospect Identification</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#service">Contact Discovery</a></li>
+            <li><a class="dropdown-item" href="#service">Data Enrichment</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#team">Our Team</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img\banner2.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-md-block">
+        <h5>Techno web finds leads you never knew existed.</h5>
+        <p class="fs-3">Work with a lead generation company that gets it.</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img\banner1.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-md-block">
+        <h5>Lead Nurturing</h5>
+        <p class="fs-3">Generating leads is just the first step in the sales process</p>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="img\banner.jpg" class="d-block w-100" alt="...">
+      <div class="carousel-caption d-md-block ">
+        <h5>Engagement Programs</h5>
+        <p class="fs-3">To truly maximize your lead generation efforts, you need a comprehensive engagement strategy.</p>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</header>
+
+<!---------------------------Abot me------------------------------>
+
+<section class="about my-5" id="about">
+  <div class="container">
+    <div class="text-center my-5">
+      <h1 data-aos="fade-up" data-aos-offset="200">About <span class="text-primary">us</span></h1>
+      <hr/ class="w-25 m-auto">
+    </div>
+    <div class="row">
+      <div class="col-sm-12 text-center" data-aos="zoom-in" data-aos-offset="200">
+        <h1>You are Unique,<span class="text-primary">So are we!</span></h1>
+        <p class="p-2">
+        Techno-web huenables B2B technology companies to increase global reach and engagement. We do the heavy lifting to realize your deeply specific marketing needs, with a professional mix of tactics, technologies, and a personal touch. We are a credible, risk-free, and flexible demand management partner for B2B technology companies and marketing agencies across the world.</br></br>        Since 2009, we have served to lead B2B technology companies, media agencies, and publishers globally. We offer a deeper level of relationship and operational efficiency in order to realize your customer acquisition requirements. We have pioneered in offering prospect data and intelligence, event participation management, lead generation, lead qualification, lead nurturing, account-based marketing, content marketing, and marketing automation support to companies desiring enhanced marketing and sales capabilities. 
+        </p>
+        <hr/>
+      </div>
+    </div>
+  </div>
+<div class="row">
+    <div class="col-sm-12 col-md-6 col-lg-6 col-6 my-5" data-aos="fade-right" data-aos-offset="200">
+      <div class="card-body ms-5">
+        <i class="fa fa-building mb-3 text-primary fs-4"></i>
+        <h5 class="card-title mb-5 fs-2">COMPANY OVERVIEW</h5>
+        <p class="card-text">Established in 2009, Techno web has been proudly serving leading B2B technology companies, media agencies, and publishers across the globe. We use AI and a close-loop data feedback system in order to realize your customer acquisition requirements.</p>
+      </div> 
+    </div>
+
+    <div class="col-sm-12 col-md-6 col-lg-6 col-12 m-auto" data-aos="fade-left" data-aos-offset="200">
+      <div class="card-body">
+        <i class="fa fa-globe mb-3 text-primary fs-4"></i>
+        <h5 class="card-title mb-5 fs-2">GLOBAL REACH INCLUDES</h5>
+        <p class="card-text">North America, Europe, the Middle East, Africa, South Asia, and Australia, we are the first choice of top media agencies, publishers, and B2B businesses to generate demand with the fastest IO to lead delivery amongst all our competitors by using seamless automated lead delivery.</p>
+      </div> 
+    </div>
+</div>
+</section>
+
+<!-----------------------Services---------------------------->
+
+<section class="sevices py-5 text-center" id="service">
+<div class="container bg-light">
+  <div class="text-center my-5">
+    <h1 data-aos="fade-up" data-aos-offset="200">Our <span class="text-primary">Sevices</span></h1>
+    <hr/ class="w-25 m-auto">
+  </div>
+  <div class="row">
+        <div class="col-sm-12 col-md-3 col-lg-3 col-12" data-aos="fade-up" data-aos-offset="200">
+            <img src="img\service1.jpg" class="card-img-top mb-3" alt="...">
+            <h5 class="card-title">Performance Marketing</h5>
+            <div class="card-body">
+              <p class="card-text">Performance Marketing is a results-oriented approach that focuses on generating measurable outcomes and driving meaningful actions. Unlike traditional marketing methods, Performance Marketing emphasizes accountability and effectiveness. We believe that every marketing effort should deliver a quantifiable return on investment (ROI) and contribute to your overall business objectives.</p>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3 col-12" data-aos="fade-down" data-aos-offset="200">
+            <img src="img/service2.jpg" class="card-img-top mb-3" alt="...">
+            <h5 class="card-title">Prospect Identification</h5>
+            <div class="card-body">
+              <p class="card-text">Our Prospect Identification Service is designed to help you find the perfect prospects for your products or services. With our advanced data analysis techniques and comprehensive databases, we can identify high-potential leads that match your target audience, saving you time and effort in the process.</p>            </div>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3 col-12" data-aos="fade-up" data-aos-offset="200">
+            <img src="img\service3.jpg" class="card-img-top mb-3" alt="...">
+            <h5 class="card-title">Contact Discovery</h5>
+            <div class="card-body">
+              <p class="card-text">Our Contact Discovery service is designed to simplify your lead generation process and empower your sales and marketing teams with the right contacts to reach out to. Whether you are looking to expand your network, reach out to potential clients, or connect with industry experts, our Contact Discovery service is here to simplify and enhance your outreach efforts.</p>
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-3 col-lg-3 col-12 " data-aos="fade-down" data-aos-offset="200">
+            <img src="img\service4.jpg" class="card-img-top mb-3" alt="...">
+            <h5 class="card-title">Data Enrichment</h5>
+            <div class="card-body">
+              <p class="card-text">Unlock the full potential of your data by enhancing its quality, accuracy, and completeness with our comprehensive Data Enrichment service. Whether you are a small startup or a large enterprise, our data enrichment solutions can provide you with the insights you need to make informed business decisions and drive growth.</p>
+            </div>
+        </div>
+      </div>
+</div>
+</section>
+<!------------------------our team------------------------------->
+<section class="team py-5 text-center" id="team">
+  <div class="container">
+    <div class="text-center my-5">
+      <h1 data-aos="fade-up" data-aos-offset="200">Our <span class="text-primary">Team</span></h1>
+      <hr/ class="w-25 m-auto">
+    </div>    
+
+    <div class="row">
+      <div class="col-sm-12 col-md-4 col-lg-4 col-12">
+        <div class="card-body">
+          <img src="img\t1.jpg" class="img-fluid rounded-circle border border-primary p-1" height="200px" width="50%" alt="...">
+            <h5 class="card-title mt-4">MOHIT JOSHI</h5>
+            <p class="card-text">Chief Executive Officer</p>
+            <a href="#" class="btn btn-primary">Know me</a>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-4 col-lg-4 col-12">
+        <div class="card-body">
+          <img src="img\t2.jpg" class="img-fluid rounded-circle border border-primary p-1" height="200px" width="50%" alt="...">
+            <h5 class="card-title">ATUL SONEJA</h5>
+            <p class="card-text">Chief Operating Officer</p>
+            <a href="#" class="btn btn-primary">Know me</a>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-4 col-lg-4 col-12">
+        <div class="card-body">
+          <img src="img\t3.jpg" class="img-fluid rounded-circle border border-primary p-1" height="100px" width="45%" alt="...">
+            <h5 class="card-title mt-4">ATUL SONEJA</h5>
+            <p class="card-text">Chief Operating Officer</p>
+            <a href="#" class="btn btn-primary">Know me</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<!--------------------------contact------------------------------>
+
+<section class="contact py-5" id="contact">
+  <div class="container">
+    <div class="text-center my-5">
+      <h1 data-aos="fade-up" data-aos-offset="200">feel free<span class="text-primary"> to connect</span></h1>
+      <hr/ class="w-25 m-auto">
+    </div>
+ 
+    <div class="row">
+      <div class="col-sm-12 col-md-6 col-lg-6 col-12" data-aos="fade-right" data-aos-offset="200">
+        <form class="row g-3">
+          <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail4">
+          </div>
+          <div class="col-md-6">
+              <label for="inputPassword4" class="form-label">Password</label>
+            <input type="password" class="form-control" id="inputPassword4">
+          </div>
+          <div class="col-12">
+            <label for="inputAddress" class="form-label">Address</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+          </div>
+  
+          <div class="col-md-6">
+            <label for="inputCity" class="form-label">City</label>
+            <input type="text" class="form-control" id="inputCity">
+          </div>
+         
+          <div class="col-md-4">
+            <label for="inputState" class="form-label">State</label>
+            <select id="inputState" class="form-select">
+              <option selected>Choose...</option>
+              <option>...</option>
+            </select>
+          </div>
+
+          <div class="col-md-2">
+            <label for="inputZip" class="form-label">Zip</label>
+            <input type="text" class="form-control" id="inputZip">
+          </div>
+
+          <div class="col-12">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck">
+              <label class="form-check-label" for="gridCheck">
+               Check me out
+              </label>
+            </div>
+          </div>
+
+           <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </div>
+        </form>
+      </div>
+
+    <div class="col-sm-12 col-md-6 col-lg-6 col-12 m-auto text-end p-4"data-aos="fade-left" data-aos-offset="200">
+      <img src="img\t4.jpg" class="img-fluid img-thumbail border border-primary p-2" height="200px" width="200%" alt="...">
+    </div>
+
+  </div>
+</section>
+
+<!-------------------------footer----------------------->
+
+<div class="container-fluid bg-primary text-white text-center p-2 ">
+  <span class="m-5">© 2024 CandorWorks Pvt Ltd. All Rights Reserved.</br>
+  <span class="m-5">Designed at tqds.in</span>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+</body>
+</html>
